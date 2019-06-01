19b06987f3127c84ffd76affc3b85566a1aa18fa
naman kumar <namankumar821310@gmail.com>
2019-06-02 00:36:25 +0530

    Remove unnecessary call to console log

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,3 @@
-console.log('Client side javascript file is loaded!')
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-// })
-
-// fetch('http://localhost:3000/weather?address=!').then((response) => {
-//     response.json().then((data) => {
-//         if(data.error){
-//             console.log(data.error)
-//         }
-//         else{
-//             console.log(data.location)
-//             console.log(data.forecast)
-//         }
-//     })
-// })
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const msg1 = document.getElementById("msg-1")
